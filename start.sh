1e4833be3694d16a4a52162be2a371094603dc15
Scott Bigelow <epheph@gmail.com>
2019-04-17 21:28:32 -0700

    export not necessary, give crond pid 1

--- start.sh
+++ start.sh
@@ -2,11 +2,9 @@
 
 set -e
 
-export OPTIONS=$OPTIONS
-
 if [[ "$1" == 'now' ]]; then
     exec /curl.sh
 else
 	echo "$CRON_SCHEDULE /curl.sh" >> /var/spool/cron/crontabs/root
-    crond -l 2 -f
+    exec crond -l 2 -f
 fi
